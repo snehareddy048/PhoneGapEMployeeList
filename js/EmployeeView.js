c238b2c95351757021695836a37d98bf7ab19505
snehareddy048 <sneha.yeluguri@capitalone.com>
2014-11-26 13:51:34 -0500

    using contact api

--- js/EmployeeView.js
+++ js/EmployeeView.js
@@ -4,7 +4,7 @@ var EmployeeView = function(employee) {
         this.el = $('<div/>');
         this.el.on('click', '.add-location-btn', this.addLocation);
             this.el.on('click', '.add-contact-btn', this.addToContacts);
-
+    this.el.on('click', '.add-contact-btn', this.addToContacts);
     };
  this.render = function () {
 		this.el.html(EmployeeView.template(employee));
@@ -22,6 +22,23 @@ var EmployeeView = function(employee) {
         });
     return false;
 };
+this.addToContacts = function(event) {
+    event.preventDefault();
+    console.log('addToContacts');
+    if (!navigator.contacts) {
+        app.showAlert("Contacts API not supported", "Error");
+        return;
+    }
+    var contact = navigator.contacts.create();
+    contact.name = {givenName: employee.firstName, familyName: employee.lastName};
+    var phoneNumbers = [];
+    phoneNumbers[0] = new ContactField('work', employee.officePhone, false);
+    phoneNumbers[1] = new ContactField('mobile', employee.cellPhone, true); // preferred number
+    contact.phoneNumbers = phoneNumbers;
+    contact.save();
+    return false;
+};
+
 
     this.initialize();
  
