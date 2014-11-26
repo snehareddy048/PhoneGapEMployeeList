3518dca9ad1e47d0d46b684476d7de18d4298fc0
snehareddy048 <sneha.yeluguri@capitalone.com>
2014-11-26 14:28:13 -0500

    changes trail to reflect in phone

--- js/HomeView.js
+++ js/HomeView.js
@@ -1,7 +1,7 @@
 var HomeView = function(store) {
  this.render = function() {
     this.el.html(HomeView.template());
-    return this; 
+    return this;  
 };
 this.findByName = function() {
     store.findByName($('.search-key').val(), function(employees) {
